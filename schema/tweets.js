bf0535eea90ecb255722a00c18ce169988f52c91
jayanthanala <javaoneapl@gmail.com>
2020-07-22 13:15:15 +0530

    new schema

--- schema/tweets.js
+++ schema/tweets.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const tweetSchema = new mongoose.Schema({
   userHead:String,
+  username:String,
   content:String,
-  userId:String
+  userId:String,
+  time:String,
+  date:String
 });
 
 const Tweet = mongoose.model('Tweet',tweetSchema);
